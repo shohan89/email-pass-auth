8712a60f0e91e92bf872f5bde3bf626b985263d6
shoh4n <maffin.killer007@gmail.com>
2025-08-21 12:43:23 +0600

    create simple form and consol log from data when submit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,26 @@
 
 const Register = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
     return (
-        <div>
-            <h2>This is register</h2>
+        <div className=" mx-auto w-96">
+            <form onSubmit={handleSubmit}>
+                <fieldset className="fieldset border-base-300 rounded-box w-xs border p-4">
+                <legend className="fieldset-legend">Register</legend>
+
+                <label className="label">Email</label>
+                <input type="email" name="email" className="input" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" name="password" className="input" placeholder="Password" />
+
+                <button className="btn btn-neutral mt-4">Register</button>
+            </fieldset>
+            </form>
         </div>
     );
 };
