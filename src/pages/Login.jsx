7166429a15edb0625a536dc0202bb8371640eaee
shohan89 <maffin.killer007@gmail.com>
2025-08-22 20:37:58 +0600

    set password reset link

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,12 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { useRef, useState } from "react";
 import { Link } from "react-router";
 import auth from "../firebase/firebase.init";
 
 const Login = () => {
     const [loginError, setLogInError] = useState('');
     const [loginSuccess, setLogInSuccess] = useState('');
+    const emailRef = useRef(null);
     const handleLogIn = e => {
         e.preventDefault();
         const email = e.target.email.value;
@@ -24,6 +25,30 @@ const Login = () => {
                 setLogInError(error.message);
             })
     }
+    // handle forget password
+    const handleForgetPassword = () => {
+        const email = emailRef.current.value;
+        console.log("🚀 ~ handleForgetPassword ~ email:", email);
+        // validate email before sending reset link
+        if(!email){
+            setLogInError('Please enter your email to reset password!');
+            return;
+        }
+        else if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)){
+            setLogInError("Please enter a valid email address!");
+            return;
+        }
+        setLogInError('') // reset error state on each render
+
+        // send password reset email
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                setLogInSuccess("Password reset email sent! Please check your email.");
+            })
+            .catch(error => {
+                setLogInError(error.message);
+            })
+    }
     return (
         <div className=" mx-auto w-96">
             <form onSubmit={handleLogIn}>
@@ -31,7 +56,12 @@ const Login = () => {
                 <legend className="fieldset-legend">Log In</legend>
 
                 <label className="label">Email</label>
-                <input type="email" name="email" className="input" placeholder="Email" required />
+                <input 
+                ref={emailRef}
+                type="email" 
+                name="email" 
+                className="input" 
+                placeholder="Email" required />
 
                 <label className="label">Password</label>
                 <input 
@@ -40,7 +70,7 @@ const Login = () => {
                 className="input" 
                 placeholder="Password" 
                 required />
-                <label className="label">
+                <label onClick={handleForgetPassword} className="label">
                     <span className="label-text-alt link link-hover">Forget Password</span>
                 </label>
 
